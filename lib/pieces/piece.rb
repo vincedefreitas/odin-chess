462a3736dd0d3598ea04352ac84292c311ab169f
Vince De Freitas <97405426+vincedefreitas@users.noreply.github.com>
2023-03-03 12:10:33 +0000

    Create possible moves method

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,7 +1,7 @@
 require_relative '../board.rb'
 
 class Piece
-  attr_accessor :current_square, :symbol, :colour, :current_square_colour, :start_square, :moves
+  attr_accessor :current_square, :symbol, :colour, :current_square_colour, :start_square, :move_list, :past_moves
 
   def initialize
     @board = Board.new
@@ -10,11 +10,30 @@ class Piece
     @current_square = nil
     @current_square_colour = nil
     @start_square = nil
-    @moves = []
+    @move_list = []
+    @past_moves = []
+    @moved = false
   end
 
   def update_current_square(square)
+    @moved = true
+    @past_moves << square
     @current_square = square
   end
+
+  def update_square_colour(colour)
+    @current_square_colour = colour
+  end
+
+  def possible_moves
+    current = @board.square_to_arr(@current_square)
+    possible_moves_arr = []
+    @move_list.each do |move|
+      possible_moves_arr << @board.arr_to_square([move[0] + current[0], move[1] + current[1]])
+    end
+    possible_moves_arr
+  end
+
+
 
 end
--- spec/pieces/piece_spec.rb
+++ spec/pieces/piece_spec.rb
@@ -11,5 +11,21 @@ describe Piece do
     end
   end
 
-  describe '#current_square' do 
+  describe '#update_square_colour' do
+    it 'updates the @current_square_colour variable' do
+      piece.update_square_colour('black')
+      result = piece.current_square_colour
+      expect(result).to eq('black')
+    end
+  end
+
+  describe '#possible_moves' do
+    it 'provides an array of all possible moves from current square' do
+      piece.move_list = [[1, 1], [2, 2]]
+      piece.current_square = 'c5'
+      result = piece.possible_moves
+      expect(result).to eq(['d6', 'e7'])
+    end
+  end
+
 end
